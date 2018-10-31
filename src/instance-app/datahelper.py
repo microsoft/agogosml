992dba541c4be4b7ad7f6e545a9bca47e880516e
margaretmeehan <mkm248@cornell.edu>
2018-10-31 17:06:07 -0400

    Addressed Elad's Comments: Added in memory schema storage and linting

--- src/instance-app/datahelper.py
+++ src/instance-app/datahelper.py
@@ -1,24 +1,31 @@
 import json
 from jsonschema import validate
 
-def validate_schema(data: object):  
+in_memory_files = {}
+
+
+def validate_schema(data: object, schema_filepath: str):
     """ Validates the input json data against our schema
         defined in schema_example.json
 
         Args: serialized json object that server has retrieved
-    """  
-    with open('schema_example.json', 'r') as f:
-        schema_data = f.read()
-    sample_schema = json.loads(schema_data)
+    """
+    if schema_filepath not in in_memory_files.keys():
+        print("it's not in memory")
+        with open(schema_filepath, 'r') as schema:
+            schema_data = schema.read()
+        sample_schema = json.loads(schema_data)
+        in_memory_files[schema_filepath] = sample_schema
 
     parsed_json = json.loads(data)
-    validate(parsed_json, sample_schema)
+    validate(parsed_json, in_memory_files[schema_filepath])
+
 
 def transform(data: object):
-    """ Applies simple transformation to the data for the 
+    """ Applies simple transformation to the data for the
         final output
 
-        Args: 
+        Args:
             data: serialized json object that has been validated against schema
     """
     parsed_json = json.loads(data)
--- src/instance-app/testapp.py
+++ src/instance-app/testapp.py
@@ -1,31 +1,35 @@
-
-import datahelper
+import os
 import json
+from dotenv import load_dotenv
 from hypothesis import given
 from hypothesis.strategies import fixed_dictionaries, text, characters, integers
+import datahelper
 
-#TO DO: Make Unit Tests More Robust
+load_dotenv()
+SCHEMA_FILEPATH = os.getenv('SCHEMA_FILEPATH')
+
+# TO DO: Make Unit Tests More Robust
+
 
 @given(data=fixed_dictionaries({
-        'key': text(characters()),
-        'intValue': integers()
-        }))
+    'key': text(characters()),
+    'intValue': integers()
+}))
 def test_validation(data):
+    """ Test that the schema validator is working as expected """
     encoded_json = json.dumps(data)
-    assert datahelper.validate_schema(encoded_json) is None
+    assert datahelper.validate_schema(encoded_json, SCHEMA_FILEPATH) is None
+
 
 @given(data=fixed_dictionaries({
-        'key': text(characters()),
-        'intValue': integers()
-        }))
+    'key': text(characters()),
+    'intValue': integers()
+}))
 def test_transform(data):
+    """ Test the simple transformation on the input data """
     encoded_original_json = json.dumps(data)
     transformed_object = datahelper.transform(encoded_original_json)
 
     data['intValue'] = data['intValue'] + 100
-    
-    assert transformed_object == data
 
-if __name__ == '__main__':
-    test_validation()
-    test_transform()
+    assert transformed_object == data

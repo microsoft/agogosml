725640df16193812393a904c9e47b0a33ffe4120
devlace <lace.lofranco@microsoft.com>
2018-11-01 22:09:23 +1100

    Add cli generate command scaffolding and first test

--- agogosml_cli/tests/test_cli_generate.py
+++ agogosml_cli/tests/test_cli_generate.py
@@ -2,24 +2,63 @@
 
 """Tests for `agogosml_cli` package."""
 
-# from click.testing import CliRunner
-# import cli.generate as generate
+import os
+import json
+from click.testing import CliRunner
+import cli.generate as generate
 
 
 def test_generate_schema():
     print("TEST SCHEMA VALIDATION BY GIVING IT A VALID AND INVALID SCHEMA...")
 
 
+"""
+http://click.palletsprojects.com/en/7.x/testing/
+You want to test the ff. commands (lets start w/ one test case for now):
+* agogosml generate
+    * should fail if no manifest.json is in working directory
+    * should fail if manifest.json is invalid
+    * should fail if files we expect to generate already exist.
+        Should it give warning and ask to overwrite?
+    * should generate .env file, datapipeline.yml, cicd.yml, Pipfile,
+        tests/e2e/, tests/validation exist
+    * should fail if any yml file is not valid yaml.
+"""
+
 def test_generate():
     """
-    http://click.palletsprojects.com/en/7.x/testing/
-    You want to test the ff. commands (lets start w/ one test case for now):
-    * agogosml generate
-        * should fail if no manifest.json is in working directory
-        * should fail if manifest.json is invalid
-        * should fail if files we expect to generate already exist.
-            Should it give warning and ask to overwrite?
-        * should generate .env file, datapipeline.yml, cicd.yml, Pipfile,
-            tests/e2e/, tests/validation exist
-        * should fail if any yml file is not valid yaml.
+    RUN: agogosml generate
+    RESULT: Produces the ff in the current working directory:
+        - .env
+        - datapipeline.yml
+        - cicd.yml
+        - Pipfile
+        - test/e2e
+        - tests/validation
     """
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        manifest_str = """
+        {
+            "name": "my-data-pipeline",
+            "tests": [{
+                "name": "Sanity Check",
+                "type": "language-specific",
+                "input": "in.json",
+                "output": "out.json",
+                "outputFormatter": "ConsoleOutputFormatterClass"
+            }]
+        }
+        """
+        manifest = json.loads(manifest_str)
+        with open('manifest.json', 'w') as f:
+            json.dump(manifest, f, indent=4)
+
+        result = runner.invoke(generate.generate)
+        assert result.exit_code == 0
+        assert os.path.exists('./.env')
+        assert os.path.exists('./datapipeline.yml')
+        assert os.path.exists('./cicd.yml')
+        assert os.path.exists('./Pipfile')
+        assert os.path.exists('./test/e2e')
+        assert os.path.exists('./test/validation')

c54e96adae9b8b88474f20cb4b74f01acb803b6b
morsh <mor.shemesh@gmail.com>
2018-11-02 12:25:49 +0200

    Fix example

---
+++ deployment/variables.network.tf
@@ -0,0 +1,21 @@
+variable nsg_name {
+    description = "Network security group name"
+    default = "1-nsg"
+}
+
+variable vnet_name {
+    description = "virtual network name"
+    default = "1-vnet"
+}
+
+variable subnet_name {
+    default = "1-subnet"
+}
+
+variable address_space {
+  default = "10.1.0.0/16"
+}
+
+variable address_prefix {
+  default = "10.1.0.0/24"
+}

bcd5783f62d088b036a977b08bde575896bc5a0d
Federica Nocera <federica.nocera@gmail.com>
2018-11-02 11:33:46 -0400

    Updated with docstrings

--- input-reader/eventhub_client_broker.py
+++ input-reader/eventhub_client_broker.py
@@ -6,6 +6,17 @@ class EventHubClientBroker(AbstractClientBroker):
                  address='': str,
                  user='': str,
                  key='': str):
+        """
+        Class to create an eventhub client broker instance.
+
+        Args:
+            address: Address string can be in either of these formats:
+                    "amqps://<URL-encoded-SAS-policy>:<URL-encoded-SAS-key>@<mynamespace>.servicebus.windows.net/myeventhub"
+                    "amqps://<mynamespace>.servicebus.windows.net/myeventhub".
+            user: User account string.
+            key: A name space SAS key string.
+
+        """
         if address == '' or user == '' or key == '':
             raise Exception('''
             Must include address, user, key for eventhub!
@@ -13,23 +24,45 @@ class EventHubClientBroker(AbstractClientBroker):
         self.client = EventHubClient(address, username=user, password=key)
 
     def create_topic(self, topic):
+        """
+        Creates a topic in eventhub.
+
+        Args:
+            topic: A string topic.
+
+        """
         # add in functionality for Eventhub client
         pass
 
     def get_producer(self, *args, **kwargs):
+        """
+        Creates producer object.
+        """
         self.producer = self.client.add_sender(*args, **kwargs)
 
     def get_consumer(self, *args, **kwargs):
+        """
+        Creates consumer object.
+        """
         self.consumer = self.client.add_receiver(*args, **kwargs)
 
     def mutate_message(self, message: str):
+        """
+        Mutates input message.
+        """
         return EventData(message)
 
 
     async def send(self, message: str, *args, **kwargs):
+        """
+         Upload a message to a eventhub topic.
+
+        Args:
+            message: A string input to upload to eventhub.
+        """
         '''
         TODO:
-        We are going to need documentation for kafka & Eventhub
+        We are going to need documentation forEventhub
         to ensure proper syntax is clear
 
         '''
@@ -46,9 +79,12 @@ class EventHubClientBroker(AbstractClientBroker):
 
 
     async def receive(self, message: str, *args, **kwargs):
+        """
+        Receive messages from a eventhub topic.
+        """
         '''
         TODO:
-        We are going to need documentation for kafka & Eventhub
+        We are going to need documentation for Eventhub
         to ensure proper syntax is clear
 
         '''
--- input-reader/kafka_client_broker.py
+++ input-reader/kafka_client_broker.py
@@ -3,6 +3,20 @@ from confluent_kafka import Producer, Consumer, admin
 
 class KafkaClientBroker(AbstractClientBroker):
     def __init__(self, config, topic):
+        """
+        Class to create a kafka client broker instance.
+
+        Args:
+            config: A dict config file with following structure:
+                    config = {
+                                'bootstrap.servers': '127.0.0.1:9092',
+                                'group.id': 'group1',
+                                'enable.auto.commit': False
+                            }
+                    Must specify "bootstrap.servers" and "group.id".
+                    Docs here https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+            topic: A string kafka topic.
+        """
         self.config = config
         self.topic = topic
         if self.is_empty(self.config):
@@ -22,6 +36,9 @@ class KafkaClientBroker(AbstractClientBroker):
         self.consumer = Consumer(self.config)
 
     def topic_exists(self, topic):
+        """
+        Verifies whether a kafka topic already exists.
+        """
         result = self.admin.create_topics(
             [admin.NewTopic(topic, 1)],
             validate_only=True
@@ -29,35 +46,48 @@ class KafkaClientBroker(AbstractClientBroker):
         return result[topic].result() is not None
 
     def create_topic(self, topic, max_partitions=1):
+        """
+        Creates a new kafka topic given a string input name
+
+        Args:
+            topic: A string input.
+            max_partitions: Maximum number of partitions to use.
+        """
         if not self.topic_exists(topic):
             self.admin.create_topics([admin.NewTopic(topic, max_partitions)])
 
     def mutate_message(self, message: str):
+        """
+        Mutate the input string message.
+
+        Args:
+            message: A string input.
+        """
         return message.encode('utf-8')
 
     async def send(self, message: str, *args, **kwargs):
-        '''
-        TODO:
-        We are going to need documentation for kafka & Eventhub
-        to ensure proper syntax is clear
+        """
+        Upload a message to a kafka topic.
 
-        '''
+        Args:
+            message: A string input to upload to kafka.
+        """
         if not isinstance(message, str):
             raise TypeError('str type expected for message')
         mutated_message = self.mutate_message(message)
         self.producer.poll(0)
         self.producer.produce(self.mutated_message, *args, **kwargs)
 
-    async def receive(self, message: str, *args, **kwargs):
+    async def receive(self, *args, **kwargs):
+        """
+        Receive messages from a kafka topic.
+        """
         '''
         TODO:
-        We are going to need documentation for kafka & Eventhub
+        We are going to need documentation for Kafka
         to ensure proper syntax is clear
 
         '''
-        if not isinstance(message, str):
-            raise TypeError('str type expected for message')
-        mutated_message = self.mutate_message(message)
         self.consumer.subscribe([self.topic])
         while True:
             # NEED OFFSETS/CHECKPOINTS!!!
@@ -77,7 +107,8 @@ class KafkaClientBroker(AbstractClientBroker):
 
     def is_empty(dictionary: dict) -> bool:
         """
-        empty dictionaries resolve to false when
+        Checks if a dictionary is empty.
+        Empty dictionaries resolve to false when
         converted to booleans in Python.
         """
         return not bool(dictionary)

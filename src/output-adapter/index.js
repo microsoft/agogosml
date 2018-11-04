d833ef9b9085cbd7aa0fed13636cc21d15d46b46
Elad Iwanir <eladiw@users.noreply.github.com>
2018-11-04 10:59:34 +0200

    adding linting for sample apps (input & output) and adding exact location of docker compose files

--- src/output-adapter/index.js
+++ src/output-adapter/index.js
@@ -1,26 +1,25 @@
-const express = require('express')
+const express = require('express');
 const https = require('http');
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
 app.get('/', (req, res) => {
-    console.log('Output adapter!')
-    res.send('Output adapter!')
-})
+    console.log('Output adapter!');
+    res.send('Output adapter!');
+});
 
 var server = app.listen(port, () => console.log(`Output adapter app listening on port ${port}!`))
 
 app.get('/shutdown', (req, res) => {
-    console.log('Output adapter is shutting down')
-    res.send('Output adapter is shutting down')
-    server.close()
-    process.exit()
-})
+    console.log('Output adapter is shutting down');
+    res.send('Output adapter is shutting down');
+    server.close();
+    process.exit();
+});
 
 function shutDownInput(arg) {
     console.log('calling shutdown for input apps');
-    https.get('http://input-adapter.agogosml.com:' + arg + '/shutdown')
-  }
+    https.get('http://input-adapter.agogosml.com:' + arg + '/shutdown');
+}
   
 setTimeout(shutDownInput, 5000, 3000);
-  

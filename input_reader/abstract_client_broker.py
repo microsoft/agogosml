cc81261cdf0969012c89d63780da21c89b880e0a
Eric Schles <erschles@microsoft.com>
2018-11-05 13:07:51 -0500

    adding base class

---
+++ input_reader/abstract_client_broker.py
@@ -0,0 +1,25 @@
+"""Abstract client broker class"""
+
+from abc import ABC, abstractmethod
+
+
+class AbstractClientBroker(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def create_topic(self, topic):
+        pass
+
+
+    @abstractmethod
+    def mutate_message(self, message: str):
+        pass
+
+    @abstractmethod
+    async def send(self, message: str):
+        pass
+    
+    @abstractmethod
+    async def receive(self, message: str):
+        pass

58c0430f43ad92ff6f3f6f78e8d86a38c9733503
Federica Nocera <federica.nocera@gmail.com>
2018-11-06 14:30:04 -0600

    Moved files into new folder structure and added to pipfile (#106)
    * Moved files into new folder structure and added to pipfile
    
    * Moved to submodule.

---
+++ agogosml/agogosml/streaming_client/abstract_streaming_client.py
@@ -0,0 +1,30 @@
+"""Abstract streaming client class"""
+
+from abc import ABC, abstractmethod
+
+
+class AbstractStreamingClient(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def mutate_message(self, message: str):
+        pass
+
+    @abstractmethod
+    def get_producer(self):
+        pass
+
+    @abstractmethod
+    def get_consumer(self):
+        pass
+    
+    @abstractmethod
+    async def send(self):
+        pass
+
+    @abstractmethod
+    async def receive(self, *args, **kwargs):
+        pass
+

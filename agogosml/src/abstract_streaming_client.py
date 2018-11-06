e7d7a63b70e6370099fa3d455b547865e7b00118
Eric Schles <erschles@microsoft.com>
2018-11-06 09:45:59 -0500

    adding new folder structure

---
+++ agogosml/src/abstract_streaming_client.py
@@ -0,0 +1,29 @@
+from abc import ABC
+
+
+class KafkaClientBroker(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def mutate_message(self, message: str):
+        pass
+
+    @abstractmethod
+    def get_producer(self):
+        pass
+
+    @abstractmethod
+    def get_consumer(self):
+        pass
+    
+    @abstractmethod
+    async def send(self):
+        pass
+
+    @abstractmethod
+    async def receive(self, *args, **kwargs):
+        pass
+            
+    

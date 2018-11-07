ce975fb1a8de780c7f290823f1b268d53c7fbdd9
Margaret Meehan <margaretmeehan@Margarets-MacBook-Pro.local>
2018-11-07 11:50:13 -0500

    Initial pass at input reader based on output writer

---
+++ input_reader/main.py
@@ -0,0 +1,42 @@
+"""
+Main entry point for input reader
+"""
+import os  # temp
+from dotenv import load_dotenv  # temp
+from input_reader.input_reader_factory import InputReaderFactory
+load_dotenv()  # temp
+
+if __name__ == "__main__":
+
+    # FOR NOW, LOAD THE CONFIG VARS IN FROM .ENV
+    EVENTHUB_CONFIG = {
+        'broker': {
+            'type': 'eventhub',
+            'config': {
+                'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
+                'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
+                'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
+                'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
+                'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
+                'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
+            },
+            'args': {  # NOT SURE WHAT THIS IS FOR.
+                'topic': 'testing'
+            }}
+    }
+
+    KAFKA_CONFIG = {
+        'broker': {
+            'type': 'kafka',
+            'config': {
+                'bootstrap.servers': '127.0.0.1:9092',
+                'group.id': 'testgroup',
+                'enable.auto.commit': False
+            },
+            'args': {
+                'topic': 'testing'
+            }}
+    }
+
+    INPUT = InputReaderFactory.create(KAFKA_CONFIG)
+    INPUT.receive_messages()  # initiate receiving
---
+++ tests/test_inputReader.py
@@ -0,0 +1,2 @@
+from agogosml.agogosml.streaming_client.abstract_streaming_client import AbstractStreamingClient
+from output_writer.output_writer import OutputWriter

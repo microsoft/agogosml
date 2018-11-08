e2e08040626904e04524ec56b5dda8a8e9a9a4fa
Margaret Meehan <margaretmeehan@Margarets-MacBook-Pro.local>
2018-11-08 13:35:45 -0500

    testing import modules

--- output_writer/main.py
+++ output_writer/main.py
@@ -6,7 +6,22 @@ from output_writer.output_writer_factory import OutputWriterFactory
 if __name__ == "__main__":
 
     # Setup correct config here
-    CONFIG = {
+    EVENTHUB_CONFIG = {
+        'broker': {
+            'type': 'eventhub',
+            'config': {
+                'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
+                'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
+                'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
+                'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
+                'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
+                'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
+                'APP_HOST': os.getenv('APP_HOST'),
+                'APP_PORT': os.getenv('APP_PORT')
+            },
+            'args': {  # NOT SURE WHAT THIS IS FOR.
+                'topic': 'testing'
+            }}
     }
-    OUTPUT = OutputWriterFactory.create(CONFIG, None, None)
+    OUTPUT = OutputWriterFactory.create(EVENTHUB_CONFIG, None, None)
     OUTPUT.start_incoming_messages()

d36d5c9088509b5070a2f7c35c939fd6c6e06ad0
Margaret Meehan <margaretmeehan@Margarets-MacBook-Pro.local>
2018-11-08 11:57:35 -0500

    Adding Output Writer to New Branch

---
+++ agogosml/agogosml/streaming_client/flask_http_listener_client.py
@@ -0,0 +1,36 @@
+""" Flask client to receive messages from customer app"""
+import os
+import threading
+
+from flask import Flask, request
+
+from agogosml.agogosml.streaming_client.listener_client import ListenerClient
+
+
+class FlaskHttpListenerClient(ListenerClient):
+
+    def __init__(self, port):
+        self.port = port
+        pass
+
+    def start(self, on_message_received):
+        app = Flask(__name__)
+
+        @app.route("/", methods=["POST"])
+        def on_input():
+
+            msg = request.get_json()
+            on_message_received(msg)
+
+        app.run(port=self.port)
+        pass
+
+    def stop(self):
+        self.shutdown_server()
+        pass
+
+    def shutdown_server(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()

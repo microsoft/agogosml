01f821c8bfc79c2c9b175117f73d43c35ddb3268
Margaret Meehan <margaretmeehan@Margarets-MacBook-Pro.local>
2018-11-08 05:46:22 -0500

    removed async

--- input_reader/input_reader.py
+++ input_reader/input_reader.py
@@ -2,13 +2,13 @@
 Input Reader
 
 --> since this and the input_reader_factory class are super small, should we combine?
-or should it follow output_writer structure exactly? 
+or should it follow output_writer structure exactly?
 """
 
 from agogosml.agogosml.streaming_client.abstract_streaming_client import AbstractStreamingClient
 
 
-class InputReader: # pylint: disable=too-few-public-methods
+class InputReader:  # pylint: disable=too-few-public-methods
     """
     Accepts incoming messages and routes them to a configured output
     """
@@ -19,9 +19,9 @@ class InputReader: # pylint: disable=too-few-public-methods
         """
         self.messaging_client = streaming_client
 
-    async def receive_messages(self):  # ASYNC OR NOT ?
+    def receive_messages(self):
         """
         Start receiving messages
         :return:
         """
-        await self.messaging_client.receive()
+        self.messaging_client.receive()
--- tests/test_inputReader.py
+++ tests/test_inputReader.py
@@ -1,2 +1,25 @@
 from agogosml.agogosml.streaming_client.abstract_streaming_client import AbstractStreamingClient
-from output_writer.output_writer import OutputWriter
+from input_reader.input_reader import InputReader
+
+class ClientMessagingMock(AbstractStreamingClient):
+
+    def __init__(self):
+        pass
+
+    async def receive(self, message: str):
+        pass
+
+def test_when_instance_created():
+    cbm = ClientMessagingMock()
+    ow = OutputWriter(cbm)
+    assert ow is not None
+
+
+async def test_when_send_executing_broker_called():
+    cbm = ClientMessagingMock()
+    assert cbm.receive_called
+
+
+if __name__ == '__main__':
+    test_when_instance_created()
+    test_when_send_executing_broker_called()

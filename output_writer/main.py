34b2fa525e8913449f2b6ee7308662780f5c77e7
Itye Richter <itye.richter@microsoft.com>
2018-11-12 17:30:25 +0200

    fix configs for input and output

--- output_writer/main.py
+++ output_writer/main.py
@@ -10,23 +10,41 @@ load_dotenv()  # temp
 
 if __name__ == "__main__":
 
-    # Setup correct config here
-    EVENTHUB_CONFIG = {
-        'broker': {
-            'type': 'eventhub',
-            'config': {
-                'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
-                'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
-                'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
-                'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
-                'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
-                'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
-                'APP_HOST': os.getenv('APP_HOST'),
-                'APP_PORT': os.getenv('APP_PORT')
-            },
-            'args': {  # NOT SURE WHAT THIS IS FOR.
-                'topic': 'testing'
-            }}
-    }
-    OUTPUT = OutputWriterFactory.create(EVENTHUB_CONFIG, None, None)
+    msg_type = os.getenv("MESSAGING_TYPE")
+    CFG = None
+
+    if msg_type == 'evenhub':
+        # FOR NOW, LOAD THE CONFIG VARS IN FROM .ENV
+        CFG = {
+            'broker': {
+                'type': 'eventhub',
+                'config': {
+                    'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
+                    'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
+                    'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
+                    'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
+                    'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
+                    'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
+                    'LEASE_CONTAINER_NAME': os.getenv("LEASE_CONTAINER_NAME"),
+                    'APP_HOST': os.getenv('APP_HOST'),
+                    'APP_PORT': os.getenv('APP_PORT'),
+                    'TIMEOUT': 1
+                }
+            }
+        }
+    elif msg_type == 'kafka':
+        CFG = {
+            'broker': {
+                'type': 'kafka',
+                'config': {
+                    'KAFKA_TOPIC': os.getenv("KAFKA_TOPIC"),
+                    'KAFKA_CONSUMER_GROUP': os.getenv("KAFKA_CONSUMER_GROUP"),
+                    'APP_HOST': os.getenv("APP_HOST"),
+                    'APP_PORT': os.getenv("APP_PORT"),
+                    'KAFKA_ADDRESS': os.getenv("APP_PORT"),
+
+                }
+            }
+        }
+    OUTPUT = OutputWriterFactory.create(CFG, None, None)
     OUTPUT.start_incoming_messages()

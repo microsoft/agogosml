31de19e73ec15ade40797f6e35bea2f909881302
Itye Richter <itye.richter@microsoft.com>
2018-11-12 18:46:42 +0200

    fix configs for input and output (#120)
    * fix configs for input and output

--- input_reader/main.py
+++ input_reader/main.py
@@ -10,39 +10,43 @@ load_dotenv()  # temp
 
 if __name__ == "__main__":
 
-    # FOR NOW, LOAD THE CONFIG VARS IN FROM .ENV
-    EVENTHUB_CONFIG = {
-        'broker': {
-            'type': 'eventhub',
-            'config': {
-                'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
-                'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
-                'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
-                'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
-                'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
-                'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
-                'LEASE_CONTAINER_NAME' : os.getenv("LEASE_CONTAINER_NAME"),
-                'APP_HOST': os.getenv('APP_HOST'),
-                'APP_PORT': os.getenv('APP_PORT'),
-                'TIMEOUT': 1
-            },
-            'args': {  # NOT SURE WHAT THIS IS FOR.
-                'topic': 'testing'
-            }}
-    }
+    msg_type = os.getenv("MESSAGING_TYPE")
+    CFG = None
 
-    KAFKA_CONFIG = {
-        'broker': {
-            'type': 'kafka',
-            'config': {
-                'bootstrap.servers': '127.0.0.1:9092',
-                'group.id': 'testgroup',
-                'enable.auto.commit': False
+    if msg_type == 'eventhub':
+        # FOR NOW, LOAD THE CONFIG VARS IN FROM .ENV
+        CFG = {
+            'broker': {
+                'type': 'eventhub',
+                'config': {
+                    'AZURE_STORAGE_ACCOUNT': os.getenv("AZURE_STORAGE_ACCOUNT"),
+                    'AZURE_STORAGE_ACCESS_KEY': os.getenv("AZURE_STORAGE_ACCESS_KEY"),
+                    'EVENT_HUB_NAMESPACE': os.getenv("EVENT_HUB_NAMESPACE"),
+                    'EVENT_HUB_NAME': os.getenv("EVENT_HUB_NAME"),
+                    'EVENT_HUB_SAS_POLICY': os.getenv("EVENT_HUB_SAS_POLICY"),
+                    'EVENT_HUB_SAS_KEY': os.getenv("EVENT_HUB_SAS_KEY"),
+                    'LEASE_CONTAINER_NAME': os.getenv("LEASE_CONTAINER_NAME"),
+                    'APP_HOST': os.getenv('APP_HOST'),
+                    'APP_PORT': os.getenv('APP_PORT'),
+                    'TIMEOUT': 1
+                }
             }
         }
-    }
+    elif msg_type == 'kafka':
+        CFG = {
+            'broker': {
+                'type': 'kafka',
+                'config': {
+                    'KAFKA_TOPIC': os.getenv("KAFKA_TOPIC"),
+                    'KAFKA_CONSUMER_GROUP': os.getenv("KAFKA_CONSUMER_GROUP"),
+                    'APP_HOST': os.getenv("APP_HOST"),
+                    'APP_PORT': os.getenv("APP_PORT"),
+                    'KAFKA_ADDRESS': os.getenv("APP_PORT"),
 
-    INPUT = InputReaderFactory.create(EVENTHUB_CONFIG)
+                }
+            }
+        }
+
+    INPUT = InputReaderFactory.create(CFG)
     INPUT.receive_messages()  # initiate receiving
     print("DONE")
-

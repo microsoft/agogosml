c7ce91826e17c5c8a1fad02ab4f93948b9ba3841
Itye Richter <itye.richter@microsoft.com>
2018-11-14 07:28:00 +0200

    added UT for otput writer

--- agogosml/tests/client_mocks.py
+++ agogosml/tests/client_mocks.py
@@ -16,19 +16,31 @@ class ClientMessagingMock(AbstractStreamingClient):
 
     def receive(self, *args, **kwargs):
         pass
-
+    
+    def get_sent(self):
+        return self.sent
 
 
 class ListenerClientMock(ListenerClient):
     def __init__(self, port):
         self.callback = None
+        self.startCalled = False
+        self.stopCalled = False
 
     def start(self, on_message_received):
         self.callback = on_message_received
+        self.startCalled = True
         pass
 
     def stop(self):
+        self.stopCalled = True
         pass
 
     def mock_new_incoming_message(self):
         self.callback("{'some':'json'}")
+
+    def get_started(self):
+        return self.startCalled
+
+    def get_stopped(self):
+        return self.stopCalled

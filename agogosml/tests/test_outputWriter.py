1ad66d32b1aff24a3c3eed92dfb0d32e0dba583c
Itye Richter <itye.richter@microsoft.com>
2018-11-14 16:33:25 +0200

    Added Unit Test for OutputWriter (#130)

--- agogosml/tests/test_outputWriter.py
+++ agogosml/tests/test_outputWriter.py
@@ -3,66 +3,40 @@ import pytest
 from agogosml.streaming_client.abstract_streaming_client import AbstractStreamingClient
 from agogosml.streaming_client.listener_client import ListenerClient
 from agogosml.writer.output_writer import OutputWriter
+from .client_mocks import ClientMessagingMock, ListenerClientMock
 
 
-# Can't move to separate file due to errors I can't solve.
+@pytest.fixture
+def MockStreamingClient():
+    return ClientMessagingMock()
 
 
-class ClientMessagingMock(AbstractStreamingClient):
-
-    def __init__(self):
-        self.sent = False
-        pass
-
-    def send(self, *args, **kwargs):
-        self.sent = True
-        pass
-
-    def close_send_client(self, *args, **kwargs):
-        pass
-
-    def receive(self, *args, **kwargs):
-        pass
-
-    def get_sent(self):
-        return self.sent
+@pytest.fixture
+def MockListenerClient():
+    return ListenerClientMock(0)
 
 
-class ListenerClientMock(ListenerClient):
-
-    def __init__(self, port):
-        self.callback = None
-
-    def start(self, on_message_received):
-        self.callback = on_message_received
-        pass
-
-    def stop(self):
-        pass
-
-    def mock_new_incoming_message(self):
-        self.callback("{'some':'json'}")
-
-
-def test_when_ctor_instance_created():
-    cbm = ClientMessagingMock()
-    clm = ListenerClientMock(0)
-    ow = OutputWriter(cbm, clm)
+def test_when_ctor_instance_created(MockStreamingClient, MockListenerClient):
+    ow = OutputWriter(MockStreamingClient, MockListenerClient)
     assert ow is not None
 
 
-def test_on_message_received_sent_called():
-    cbm = ClientMessagingMock()
-    clm = ListenerClientMock(0)
-    ow = OutputWriter(cbm, clm)
+def test_on_message_received_sent_called(MockStreamingClient, MockListenerClient):
+    ow = OutputWriter(MockStreamingClient, MockListenerClient)
     ow.on_message_received('test')
-    assert cbm.get_sent()
+    assert MockStreamingClient.get_sent()
 
 
-def test_on_listener_event_sent_called():
-    cbm = ClientMessagingMock()
-    clm = ListenerClientMock(0)
-    ow = OutputWriter(cbm, clm)
+def test_on_listener_event_sent_called(MockStreamingClient, MockListenerClient):
+    ow = OutputWriter(MockStreamingClient, MockListenerClient)
     ow.start_incoming_messages()
-    clm.mock_new_incoming_message()
-    assert cbm.get_sent()
+    MockListenerClient.mock_new_incoming_message()
+    assert MockStreamingClient.get_sent()
+    assert MockListenerClient.get_started()
+
+
+def test_on_stop_event_stop_called(MockStreamingClient, MockListenerClient):
+    ow = OutputWriter(MockStreamingClient, MockListenerClient)
+    ow.start_incoming_messages()
+    ow.stop_incoming_messages()
+    assert MockListenerClient.get_stopped()

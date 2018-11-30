581c77bbf279644b70f2a76d579c69758ac07cff
margaretmeehan <mkm248@cornell.edu>
2018-11-30 13:07:56 +1300

    Loading pipeline and make prediction based on data

--- sample_apps/mleap/mleap_serving/build.sbt
+++ sample_apps/mleap/mleap_serving/build.sbt
@@ -5,5 +5,6 @@ libraryDependencies ++= Seq(
   "com.github.finagle" %% "finch-circe" % "0.22.0",
   "io.circe" %% "circe-generic" % "0.9.0",
   "com.google.code.gson" % "gson" % "2.8.0",
-  "org.scalaj" %% "scalaj-http" % "2.4.1"
+  "org.scalaj" %% "scalaj-http" % "2.4.1",
+  "ml.combust.mleap" %% "mleap-runtime" % "0.12.0"
 )
--- sample_apps/mleap/mleap_serving/src/main/scala/com/Microsoft/mleap_serving/Main.scala
+++ sample_apps/mleap/mleap_serving/src/main/scala/com/Microsoft/mleap_serving/Main.scala
@@ -6,16 +6,34 @@ import io.finch.syntax._
 import io.circe.generic.auto._
 import com.google.gson.Gson
 import scalaj.http.Http
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.runtime.MleapSupport._
+import resource._
+
+// create a simple LeapFrame to transform
+import ml.combust.mleap.runtime.frame.{DefaultLeapFrame, Row}
+import ml.combust.mleap.core.types._
+
 
 object Main extends App {
 
   val port = sys.env("PORT")
   val output_url = sys.env("OUTPUT_URL")
+  val model_path = sys.env("MODEL_PATH")
+  val pipeline_path = "jar:file:" + model_path
+  //Users/margaretmeehan/Desktop/worktings/ASB/agogosml/sample_apps/mleap/frank_model_export_json.zip"
 
-  case class Message(key: String, value: String)
+  //MLEAP CODE TO MOVE TO SEPARATE CLASS
+
+  val bundle = (for(bundleFile <- managed(BundleFile(pipeline_path))) yield {
+    bundleFile.loadMleapBundle().get
+  }).opt.get
+
+  case class InputMessage(text: String)
+  case class OutputMessage(text: String, prediction: Double)
   case class Response(message: String)
 
-  def sendPostRequest(body: Message): String ={
+  def sendPostRequest(body: OutputMessage): String = {
     val stringBody = new Gson().toJson(body)
     val response = scalaj.http.Http(output_url).postData(stringBody)
       .header("Content-Type", "application/json")
@@ -23,18 +41,32 @@ object Main extends App {
     return response.body
   }
 
+  def transformMessage(message: InputMessage): OutputMessage = {
+    val schema = StructType(StructField("text", ScalarType.String)).get
+    val data = Seq(Row(message.text))
+    val frame = DefaultLeapFrame(schema, data)
+
+    // transform the dataframe using our pipeline
+    val mleapPipeline = bundle.root
+    val frame_transformed = mleapPipeline.transform(frame).get
+
+    val prediction_frame = frame_transformed.select("prediction").get
+    val transformedMessage = OutputMessage(message.text, prediction_frame.dataset(0).getDouble(0))
+    return transformedMessage
+  }
+
   val receiveMessage: Endpoint[Response] =
-    post("message" :: jsonBody[Message]) { message: Message =>
+    post("message" :: jsonBody[InputMessage]) { message: InputMessage =>
       // feed msg into mleap model
+      val transformedMessage = transformMessage((message))
 
-      // send the result from the model to the output
+      // send the transformed data to the output writer
+      val r = sendPostRequest(transformedMessage)
 
-      // if all succeeds, send the response from the output container
-      val r = sendPostRequest(message)
+      // send OK response to the input container if OK. Otherwise, handle exception
       Ok(Response(r))
     }.handle {
       case e: Exception =>
-        println(e)
         BadRequest(e)
     }
 

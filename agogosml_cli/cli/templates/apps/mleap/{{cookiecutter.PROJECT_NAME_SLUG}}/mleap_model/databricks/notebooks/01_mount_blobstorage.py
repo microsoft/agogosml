5095d66e2c6ccda4144809e81ff0f250b83cc804
Rami Sayar <rami.sayar@microsoft.com>
2018-12-27 08:58:20 -0500

    Fixed lint errors.

--- agogosml_cli/cli/templates/apps/mleap/{{cookiecutter.PROJECT_NAME_SLUG}}/mleap_model/databricks/notebooks/01_mount_blobstorage.py
+++ agogosml_cli/cli/templates/apps/mleap/{{cookiecutter.PROJECT_NAME_SLUG}}/mleap_model/databricks/notebooks/01_mount_blobstorage.py
@@ -1,13 +1,13 @@
 # Retrieve storage credentials
-storage_account = dbutils.secrets.get(scope = "storage_scope", key = "storage_account")
-storage_key = dbutils.secrets.get(scope = "storage_scope", key = "storage_key")
+storage_account = dbutils.secrets.get(scope="storage_scope", key="storage_account")  # noqa: F821
+storage_key = dbutils.secrets.get(scope="storage_scope", key="storage_key")  # noqa: F821
 storage_container = "databricks"
 
 # Mount
-dbutils.fs.mount(
-  source = "wasbs://databricks@" + storage_account + ".blob.core.windows.net",
-  mount_point = "/mnt/blob_storage/", 
-  extra_configs = {"fs.azure.account.key." + storage_account + ".blob.core.windows.net": storage_key})
+dbutils.fs.mount(  # noqa: F821
+  source="wasbs://databricks@" + storage_account + ".blob.core.windows.net",
+  mount_point="/mnt/blob_storage/",
+  extra_configs={"fs.azure.account.key." + storage_account + ".blob.core.windows.net": storage_key})
 
 # Refresh mounts
-dbutils.fs.refreshMounts()
+dbutils.fs.refreshMounts()  # noqa: F821

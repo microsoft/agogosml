77793c55498c958c32d5c3ff70f0b0a1ae923c3e
Margaret Meehan <mkm248@cornell.edu>
2019-01-30 16:39:01 -0500

    DOCUMENTATION: small updates throughout the documentation (#206)
    * DOCUMENTATION: small updates throughout the documentation
    * Variable consistency
    * Updated deployment Readme

--- env.example.sh
+++ env.example.sh
@@ -1,18 +1,19 @@
 # Copy this file to env.private.sh, and change the variables appropriately
 export INPUT_READER_NAME=input-reader
-export SAMPLE_APP_NAME=sample-app
+export APP_NAME=sample-app
 export OUTPUT_WRITER_NAME=output-writer
 export NETWORK_NAME=testnetwork
 export MESSAGING_TYPE=eventhub
 export AZURE_STORAGE_ACCOUNT=********
 export AZURE_STORAGE_ACCESS_KEY=****************************************************************************************
-export LEASE_CONTAINER_NAME=lease
+export LEASE_CONTAINER_NAME_INPUT=lease
+export LEASE_CONTAINER_NAME_OUTPUT=leaseoutput
 export EVENT_HUB_NAMESPACE=agogos-eh
-export INPUT_EVENT_HUB_NAME=test-input
-export INPUT_EVENT_HUB_SAS_POLICY=<policy-name>
-export INPUT_EVENT_HUB_SAS_KEY=********************************************
-export OUTPUT_EVENT_HUB_NAME=test-output
-export OUTPUT_EVENT_HUB_SAS_POLICY=<policy-name>
-export OUTPUT_EVENT_HUB_SAS_KEY=********************************************
+export EVENT_HUB_NAME_INPUT=test-input
+export EVENT_HUB_SAS_POLICY_INPUT=<policy-name>
+export EVENT_HUB_SAS_KEY_INPUT=********************************************
+export EVENT_HUB_NAME_OUTPUT=test-output
+export EVENT_HUB_SAS_POLICY_OUTPUT=<policy-name>
+export EVENT_HUB_SAS_KEY_OUTPUT=********************************************
 export APP_PORT=5000
 export OUTPUT_WRITER_PORT=8080

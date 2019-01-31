98614acf462fe21e8a7b32ea9fe8b1eeb77306c6
Clemens Wolff <clemens@justamouse.com>
2019-01-31 14:12:52 -0500

    Accept arbitrary streaming client configuration (#223)

--- agogosml/agogosml/common/abstract_streaming_client.py
+++ agogosml/agogosml/common/abstract_streaming_client.py
@@ -11,7 +11,7 @@ from agogosml.utils.imports import find_implementations
 
 class AbstractStreamingClient(ABC):
     @abstractmethod
-    def __init__(self, config: Dict[str, str]):
+    def __init__(self, config: dict):
         """
         Abstract Streaming Client
 
